beb4aab8594c76f7d6c4100dc6c2c87eed535472
Michael Emir <devmikemir@gmail.com>
2020-10-20 21:58:26 -0600

    Creación de hub para las demás secciones de la app.

---
+++ app/src/main/java/com/android/foodmarket/ui/catalog/CatalogViewModel.java
@@ -0,0 +1,19 @@
+package com.android.foodmarket.ui.catalog;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class CatalogViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public CatalogViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is gallery fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
