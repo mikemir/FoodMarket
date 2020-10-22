a9edc19002af8c818f3a255b1b485c4044a5994f
Michael Emir <devmikemir@gmail.com>
2020-10-21 19:01:17 -0600

    Incorporación de activities de registro y olvido contraseña.

---
+++ app/src/main/java/com/android/foodmarket/ForgotPasswActivity.java
@@ -0,0 +1,14 @@
+package com.android.foodmarket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForgotPasswActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_passw);
+    }
+}
--- app/src/main/java/com/android/foodmarket/LoginActivity.java
+++ app/src/main/java/com/android/foodmarket/LoginActivity.java
@@ -6,17 +6,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     Button btLogin;
+    TextView tvRegistrarme, tvOlvidoContrasenia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        tvRegistrarme = findViewById(R.id.tvRegistrase);
+        tvOlvidoContrasenia = findViewById(R.id.tvOlvidoContrasenia);
         btLogin = findViewById(R.id.btLogin);
+
+        tvRegistrarme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        tvOlvidoContrasenia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, ForgotPasswActivity.class);
+                startActivity(intent);
+            }
+        });
 
         btLogin.setOnClickListener(new View.OnClickListener() {
             @Override
